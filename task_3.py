e2d18abd29cb1aa6ad9530625262fefd3084ce1a
AnnaHuntley <95580639+AnnaHuntley@users.noreply.github.com>
2021-12-14 05:36:44 +0200

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -5,6 +5,8 @@ if number == 0 or number == 1:
     print(number, 'процент')
 if 1 < number < 5:
     print(number, 'процента')
-if 4 < number <= 100:
+if 4 < number <= 100 and number % 10 >= 5:
     print(number, 'процентов')
+if 4 < number <= 100 and number % 10 < 5:
+    print(number, 'процента')
 
