5df5eca67ee6b28ae6ff295d81b210a007a7840d
Anna <annapn32@gmail.com>
2021-12-14 03:58:48 +0200

    Initial commit

---
+++ task_3.py
@@ -0,0 +1,10 @@
+
+number = 3
+
+if number == 0 or number == 1:
+    print(number, 'процент')
+if 1 < number < 5:
+    print(number, 'процента')
+if 4 < number <= 100:
+    print(number, 'процентов')
+
